fc7968e27d771b3063fc96ab3a79af77426c6211
Sable <2633919784@qq.com>
2026-01-19 20:50:09 +0800

    登录页完成

--- SmartMall/src/views/Home.vue
+++ SmartMall/src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
     <h1>首页</h1>
+    <p>{{num}}</p>
   </div>
 </template>
+<script setup>
+import { ref } from "vue"
+const num = ref(0)
+</script>
+<style scoped>
+
+</style>
