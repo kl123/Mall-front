40d9d4fc8095f1e0978a4743d839925f454e4a4a
Calestises <wcc1203612@163.com>
2026-03-28 17:36:00 +0800

    Revert "后端文件上传"
    This reverts commit d0b7468b00c4b0a885f7313fea4fdf1d163aded9.

--- Cat/Cat/src/main/java/com/example/cat/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.cat.service;
-
-import com.example.cat.entity.User;
-
-public interface UserService {
-    User login(String username, String password);
-    boolean register(String username, String password);
-}
